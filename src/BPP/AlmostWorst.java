9cfe0b97968755c064b98da35de47cbb6b9278ad
sschrijver <stephanschrijver@gmail.com>
2015-04-30 12:52:08 +0200

    Correct!

--- src/BPP/AlmostWorst.java
+++ src/BPP/AlmostWorst.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BPP;
 
 import java.util.ArrayList;
 
-/**
- *
- * Studentnummer: S1079295 Naam: Rik Lugtenberg Klas: ICTM2D Vak : JAVA
- */
 public class AlmostWorst
 {
 
