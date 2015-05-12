636f9110e374d76437eefd0f35105ff5eeb6bbcd
sschrijver <stephanschrijver@gmail.com>
2015-05-12 10:23:16 +0200

    License headers weggehaald

--- src/BPP/Artikel.java
+++ src/BPP/Artikel.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BPP;
 
-/**
- *
- * Studentnummer: S1079295 Naam: Rik Lugtenberg Klas: ICTM2D Vak : JAVA
- */
 public class Artikel
 {
     
